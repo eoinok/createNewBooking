2283714402e3873adef6cbf9fdecd8fdbaa2104d
Eoin OKennedy <Eoin.okennedy@tudublin.ie>
2023-10-16 17:08:26 +0100

    added html form and createNewBooking.php

--- createNewBooking.php
+++ createNewBooking.php
@@ -1,14 +1,23 @@
 <?php 
-include("dbcon.php");
 
-$mid = $_POST['memberid']; 
-$cid = $_POST['courtid']; 
+$mid = $_POST['memberID']; 
+$cid = $_POST['courtID']; 
 $bd = $_POST['bookingDate']; 
 $st = $_POST['startTime']; 
 $et = $_POST['endTime']; 
 
+include("dbcon.php");
+
 $sql = "insert into booking(memberid,courtid,bookingDate,starttime,endtime) values ('$mid','$cid','$bd','$st','$et')";
 
-mysqli_query($conn,$sql); 
+echo $sql;
+/*
+if (mysqli_query($conn, $sql)) {
+  echo "<br>New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+*/
+
 mysqli_close($conn); 
 ?>
